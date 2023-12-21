00d1c68f1cf3ca494a1ae6845213ffd3799d71a2
Garrett Hale <Garrett.hale@yahoo.com>
2023-12-20 19:39:16 -0600

    update

--- src/App.js
+++ src/App.js
@@ -7,25 +7,35 @@ class App extends Component {
     super();
 
     this.state = {
-      name: { firstName: 'Andrei', lastName: 'Zhang'},
-      company: 'ZTM',
+      monsters: []
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => 
+      this.setState(
+        () => {
+          return {monsters: users};
+        },
+        () => {
+          console.log(this.state);
+        }
+      )
+    );
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}</p>
-          <button
-            onClick={() => {
-              this.setState({name: {firstName: 'Geofry', lastName: 'Georgey'}})
-            }}
-          >
-            Change Name
-            </button>
-        </header>
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
       </div>
     );
   }
